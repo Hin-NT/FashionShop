c145c13e6189b09dd3f772aa69510c23d6c727f3
Hin_NT <nthien04042001@gmail.com>
2024-10-15 16:37:29 +0700

    Refactor ProductService - Validate category and style in create and update methods. - Enhance logging in findById, delete, and update. - Optimize product retrieval in getAll and findProductByProductName. - Improve error handling in create, update, and delete methods. - Separate entity-to-DTO conversion for better reusability.

--- src/main/java/com/example/FashionShop/service/implement/ProductService.java
+++ src/main/java/com/example/FashionShop/service/implement/ProductService.java
@@ -4,6 +4,7 @@ import com.example.FashionShop.dto.ProductDTO;
 import com.example.FashionShop.dto.ResponseDTO;
 import com.example.FashionShop.model.Product;
 import com.example.FashionShop.model.Style;
+import com.example.FashionShop.repository.CategoryRepository;
 import com.example.FashionShop.repository.ProductRepository;
 import com.example.FashionShop.repository.StyleRepository;
 import com.example.FashionShop.service.interfaces.IProduct;
@@ -29,165 +30,183 @@ public class ProductService implements IProduct {
 
     private final StyleRepository styleRepository;
 
-    @Override
-    public ResponseEntity<ResponseDTO<List<ProductDTO>>> getAll() {
-        try {
-            List<Product> products = (List<Product>) productRepository.findAll();
-            if (products.isEmpty()) {
-                logger.info("No products found");
-                return ResponseEntity.status(HttpStatus.NO_CONTENT)
-                        .body(new ResponseDTO<>(null, "No products found"));
-            }
-            List<ProductDTO> productDTOs = this.convertEntityToDTO(products);
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new ResponseDTO<>(productDTOs, "Products retrieved successfully"));
-        } catch (Exception e) {
-            logger.error("Error occurred while fetching products: {}", e.getMessage(), e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDTO<>(null, "Error occurred while fetching products"));
-        }
-    }
+    private final CategoryRepository categoryRepository;
 
-    @Override
-    public ResponseEntity<ResponseDTO<Product>> create(Product product) {
-        String productId = Util.createIdForEntity(product.getProductName());
-        product.setProductId(productId);
-
-        LocalDateTime currentDateTime = LocalDateTime.now();
-        product.setCreatedAt(currentDateTime);
-        product.setUpdatedAt(currentDateTime);
-
-        try {
-            Product savedProduct = productRepository.save(product);
-            product.setProductId(savedProduct.getProductId());
-            logger.info("Product created successfully with ID: {}", savedProduct.getProductId());
-            return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(new ResponseDTO<>(product, "Product created successfully"));
-        } catch (Exception e) {
-            logger.error("Error occurred while creating product: {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDTO<>(null,
-                            "Error occurred while creating product: " + e.getMessage()));
-        }
-    }
-
-
-    @Override
-    public ResponseEntity<ResponseDTO<ProductDTO>> findById(Product product) {
-        if (product.getProductId() == null) {
-            logger.warn("Product ID is null");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseDTO<>(null, "Product ID is null"));
-        }
-
-        try {
-            Optional<Product> productOptional = productRepository.findById(product.getProductId());
-            if (productOptional.isPresent()) {
-                logger.info("Product found with ID: {}", product.getProductId());
-                return ResponseEntity.status(HttpStatus.OK)
-                        .body(new ResponseDTO<>(new ProductDTO(productOptional.get(), 1),
-                                "Product found successfully"));
-            } else {
-                logger.info("Product not found with ID: {}", product.getProductId());
-                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(new ResponseDTO<>(null,
-                                "Product not found with ID: " + product.getProductId()));
-            }
-        } catch (Exception e) {
-            logger.error("Error occurred while finding product: {}", e.getMessage(), e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDTO<>(null,
-                            "Error occurred while finding product: " + e.getMessage()));
-        }
-    }
-
-    @Override
-    public ResponseEntity<ResponseDTO<Product>> update(Product product) {
-        String productId = product.getProductId();
-
-        if (!productRepository.existsById(productId)) {
-            logger.warn("Product with ID: {} does not exist", productId);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new ResponseDTO<>(null,
-                            "Product with ID: " + productId + " does not exist"));
-        }
-
-        try {
-            Optional<Style> optionalStyle = Optional.ofNullable(product.getStyle())
-                    .flatMap(style -> styleRepository.findById(style.getStyleId()));
-
-            if (optionalStyle.isPresent()) {
-                product.setStyle(optionalStyle.get());
-            } else if (product.getStyle() != null) {
-                logger.warn("Style with ID: {} not found", product.getStyle().getStyleId());
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(new ResponseDTO<>(null, "Style not found"));
-            }
-
-            product.setUpdatedAt(LocalDateTime.now());
-
-            Product updatedProduct = productRepository.save(product);
-            logger.info("Product updated successfully with ID: {}", productId);
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new ResponseDTO<>(updatedProduct, "Product updated successfully"));
-        } catch (Exception e) {
-            logger.error("Error occurred while updating product: {}", e.getMessage(), e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDTO<>(null,
-                            "Error occurred while updating product: " + e.getMessage()));
-        }
-    }
-
-    @Override
-    public ResponseEntity<ResponseDTO<String>> delete(Product product) {
-        if (product == null || product.getProductId() == null) {
-            logger.warn("Product or Product ID is null when deleting product");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseDTO<>(null, "Product or Product ID is null"));
-        }
-
-        try {
-            if (productRepository.existsById(product.getProductId())) {
-                productRepository.deleteById(product.getProductId());
-                logger.info("Product with ID: {} deleted successfully", product.getProductId());
-                return ResponseEntity.status(HttpStatus.OK)
-                        .body(new ResponseDTO<>(null, "Product deleted successfully"));
-            } else {
-                logger.warn("Product not found with ID: {}", product.getProductId());
-                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(new ResponseDTO<>(null,
-                                "Product not found with ID: " + product.getProductId()));
-            }
-        } catch (Exception e) {
-            logger.error("Error occurred while deleting product with ID: {}: {}", product.getProductId(), e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDTO<>(null,
-                            "Error occurred while deleting product: " + e.getMessage()));
-        }
-    }
 
     @Override
     public ResponseEntity<ResponseDTO<List<ProductDTO>>> findProductByProductName(String productName) {
         try {
             List<Product> products = productRepository.findProductsByProductName(productName);
             if (products.isEmpty()) {
-                logger.info("Product with name {} not found", productName);
-                return ResponseEntity.status(HttpStatus.OK).body(new ResponseDTO<>(null, "Product with name " + productName + " not found"));
+                logger.info("No product found with name {}", productName);
+                return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                        .body(new ResponseDTO<>(null, "No product found with name " + productName));
             }
-            List<ProductDTO> productColors = this.convertEntityToDTO(products);
-            logger.info("Product with name {} found", productName);
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDTO<>(productColors, "Products retrieved successfully"));
+            List<ProductDTO> productDTOs = this.convertProductToProductDTO(products);
+            logger.info("Product found with name {}", productName);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseDTO<>(productDTOs, "Product found with name " + productName));
         } catch (Exception e) {
-            logger.error("Error occurred while finding product: {}", e.getMessage(), e);
+            logger.error("Error occurred while finding product by product name", e.getMessage(), e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new ResponseDTO<>(null,
-                            "Error occurred while finding product: " + e.getMessage()));
+                            "Error occurred while finding product by product name"));
         }
     }
 
-    private List<ProductDTO> convertEntityToDTO(List<Product> products) {
-        return products.stream()
-                .map(product -> new ProductDTO(product, 0))
-                .toList();
+    private List<ProductDTO> convertProductToProductDTO(List<Product> products) {
+        return products.stream().map(product -> new ProductDTO(product, 0)).toList();
     }
+
+    @Override
+    public ResponseEntity<ResponseDTO<List<ProductDTO>>> getAll() {
+        try {
+
+            List<Product> products = productRepository.findAll();
+            if (products.isEmpty()) {
+                logger.info("No product found");
+                return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                        .body(new ResponseDTO<>(null, "No product found"));
+            }
+            List<ProductDTO> productDTOs = this.convertProductToProductDTO(products);
+            logger.info("Product found");
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseDTO<>(productDTOs, "Product found"));
+        } catch (Exception e) {
+            logger.error("Error occurred while finding products");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDTO<>(null,
+                            "Error occurred while finding products" + e.getMessage()));
+        }
+    }
+
+    @Override
+    public ResponseEntity<ResponseDTO<Product>> create(Product product) {
+
+        if (product == null || product.getProductName() == null || product.getProductName().isBlank()) {
+            logger.warn("Product name is null or empty or blank");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body(new ResponseDTO<>(null, "Product name is null or empty or blank"));
+        }
+
+        if (!categoryRepository.existsById(product.getCategory().getCategoryId())) {
+            logger.warn("Category id {} does not exist", product.getCategory().getCategoryId());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseDTO<>(null, "Category id " + product.getCategory().getCategoryId() + " does not exist"));
+        }
+
+        if (!styleRepository.existsById(product.getStyle().getStyleId())) {
+            logger.warn("Style id {} does not exist", product.getStyle().getStyleId());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseDTO<>(null, "Style id " + product.getStyle().getStyleId() + " does not exist"));
+        }
+
+        String productName = Util.createIdForEntity(product.getProductName());
+        product.setProductId(productName);
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        product.setCreatedAt(currentDateTime);
+        product.setUpdatedAt(currentDateTime);
+
+        try {
+            productRepository.save(product);
+            logger.info("Product created successfully");
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(new ResponseDTO<>(product, "Product created successfully"));
+        } catch (Exception e) {
+            logger.error("Error occurred while creating product", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDTO<>(null,
+                            "Error occurred while creating product" + e.getMessage()));
+        }
+    }
+
+    @Override
+    public ResponseEntity<ResponseDTO<ProductDTO>> findById(Product product) {
+
+        if (product == null || product.getProductId() == null) {
+            logger.info("Product id is null or empty");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body(new ResponseDTO<>(null, "Product id is null or empty"));
+        }
+
+        try {
+            Optional<Product> productOptional = productRepository.findById(product.getProductId());
+            if (productOptional.isPresent()) {
+                logger.info("Product found with id {}", product.getProductId());
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(new ResponseDTO<>(new ProductDTO(productOptional.get(), 0),
+                                "Product found with id " + product.getProductId()));
+            }
+            logger.info("Product not found with id {}", product.getProductId());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ResponseDTO<>(null, "Product id not found"));
+        } catch (Exception e) {
+            logger.error("Error occurred while finding product", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDTO<>(null,
+                            "Error occurred while finding product" + e.getMessage()));
+        }
+    }
+
+    @Override
+    public ResponseEntity<ResponseDTO<Product>> update(Product product) {
+
+        if (product == null || product.getProductId() == null) {
+            logger.info("Product id is null or empty");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ResponseDTO<>(null, "Product id is null or empty"));
+        }
+
+        if (!categoryRepository.existsById(product.getCategory().getCategoryId())) {
+            logger.warn("Category id {} does not exist", product.getCategory().getCategoryId());
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body(new ResponseDTO<>(null, "Category id " + product.getCategory().getCategoryId() + " does not exist"));
+        }
+
+        if (styleRepository.existsById(product.getStyle().getStyleId())) {
+            logger.warn("Style id {} does not exist", product.getStyle().getStyleId());
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body(new ResponseDTO<>(null, "Style id " + product.getStyle().getStyleId() + " does not exist"));
+        }
+
+        try {
+            Optional<Product> productOptional = productRepository.findById(product.getProductId());
+            if (productOptional.isPresent()) {
+
+                product.setUpdatedAt(LocalDateTime.now());
+
+                productRepository.save(product);
+                logger.info("Product updated successfully with id {}", product.getProductId());
+                return ResponseEntity.status(HttpStatus.OK).body(new ResponseDTO<>(product, "Product updated successfully with id " + product.getProductId()));
+            }
+            logger.info("Product not found with id {}", product.getProductId());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDTO<>(null, "Product id not found"));
+        } catch (Exception e) {
+            logger.error("Error occurred while updating product", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDTO<>(null, "Error occurred while updating product" + e.getMessage()));
+        }
+    }
+
+    @Override
+    public ResponseEntity<ResponseDTO<String>> delete(Product product) {
+        if (product == null || product.getProductId() == null) {
+            logger.info("Product id is null or empty");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ResponseDTO<>(null, "Product id is null or empty"));
+        }
+
+        try {
+            Optional<Product> productOptional = productRepository.findById(product.getProductId());
+            if (productOptional.isPresent()) {
+                productRepository.deleteById(product.getProductId());
+                logger.info("Product found with id {}", product.getProductId());
+                return ResponseEntity.status(HttpStatus.OK).body(new ResponseDTO<>("Product deleted successfully", "Product deleted successfully"));
+            }
+            logger.info("Product not found with id {}", product.getProductId());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDTO<>(null, "Product not found"));
+        } catch (Exception e) {
+            logger.error("Error occurred while deleting product", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDTO<>(null, "Error occurred while deleting product" + e.getMessage()));
+        }
+    }
+
 }

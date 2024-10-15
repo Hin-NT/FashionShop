a66d77422b0e4c1017968bfe6179d264687d539f
Hin_NT <nthien04042001@gmail.com>
2024-10-15 16:29:14 +0700

    Refactor: replace imageUrls with ImageDTO list, productColorSizes with ProductColorSizeDTO

--- src/main/java/com/example/FashionShop/dto/ProductColorDTO.java
+++ src/main/java/com/example/FashionShop/dto/ProductColorDTO.java
@@ -1,15 +1,12 @@
 package com.example.FashionShop.dto;
 
-import com.example.FashionShop.model.Image;
 import com.example.FashionShop.model.ProductColor;
-import com.example.FashionShop.model.ProductColorSize;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
@@ -19,8 +16,8 @@ public class ProductColorDTO {
     String productColorId;
     ProductDTO productDTO;
     String colorId;
-    List<String> imageUrls;
-    List<String> productColorSizeIds;
+    List<ImageDTO> imageUrls;
+    List<ProductColorSizeDTO> productColorSizes;
 
     public ProductColorDTO(ProductColor productColor, int choose) {
         this.productColorId = productColor.getProductColorId();
@@ -29,11 +26,12 @@ public class ProductColorDTO {
 
         if (choose == 1) {
             this.imageUrls = productColor.getImageList().stream()
-                    .map(Image::getImageUrl)
-                    .collect(Collectors.toList());
-            this.productColorSizeIds = productColor.getProductColorSizeList().stream()
-                    .map(ProductColorSize::getProductColorSizeId)
-                    .collect(Collectors.toList());
+                    .map(ImageDTO::new)
+                    .toList();
+            this.productColorSizes = productColor.getProductColorSizeList().stream()
+                    .map(productColorSize -> new ProductColorSizeDTO(productColorSize, 0))
+                    .toList();
         }
     }
+
 }
